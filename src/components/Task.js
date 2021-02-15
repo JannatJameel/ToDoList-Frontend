fae6703626127361d8d8ec607af8c3f58f18f6ae
Mahmood28 <mahmoodalwatani@gmail.com>
2021-02-15 09:03:39 +0300

    minor changes to days output.

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,13 +6,12 @@ import CheckBox from "./Buttons/CheckBox";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 
-
 const Task = ({ task }) => {
   const days =
     (new Date(task.deadline).getTime() - new Date().getTime()) / 86400000;
   const time = (days % 1) * 24;
 
-  let passed = days<=0;
+  let passed = Math.ceil(days) <= 0;
 
   return (
     <TableRow key={task.name}>
@@ -20,16 +19,17 @@ const Task = ({ task }) => {
         {task.name}
       </TableCell>
       <TableCell align="right">
-        <CheckBox taskId={task.id} status={task.status} passed={passed}/>
+        <CheckBox taskId={task.id} status={task.status} passed={passed} />
       </TableCell>
       <TableCell>
-        due in {(days <= 0)? 0 : Math.ceil(days)} days
+        due in {days < 0 ? 0 : Math.ceil(days)} day
+        {Math.ceil(days) !== 1 ? "s" : null}
       </TableCell>
       <TableCell align="right">
-        <DeleteButton taskId={task.id} passed={passed}/>
+        <DeleteButton taskId={task.id} passed={passed} />
       </TableCell>
       <TableCell align="right">
-        <Priority taskId={task.id} passed={passed}/>
+        <Priority taskId={task.id} passed={passed} />
       </TableCell>
     </TableRow>
   );
